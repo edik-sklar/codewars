b79fdc9b328a461f04ad503ab74e25fdc71d8680
esklar <edik.sklar@gmail.com>
2025-09-25 16:09:55 -0500

    add check if array has less than 2 elements

--- sum_of_differences.py
+++ sum_of_differences.py
@@ -1,7 +1,7 @@
 
 def sum_of_differences(arr):
     arr = sorted(arr, reverse=True)
-    return sum(arr[i] - arr[i+1] for i in range(len(arr)-1))
+    return sum(arr[i] - arr[i+1] for i in range(len(arr)-1)) if len(arr) > 1 else 0
 
 
 
