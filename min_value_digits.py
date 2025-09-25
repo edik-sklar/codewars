cb05ed6be2e060abeb87ef350002f30369fcb418
esklar <edik.sklar@gmail.com>
2025-09-25 14:13:12 -0500

    fixed vasr ref

--- min_value_digits.py
+++ min_value_digits.py
@@ -1,15 +1,15 @@
-#Given a list of digits, return the smallest number that could be formed from these digits,
+# Given a list of digits, return the smallest number that could be formed from these digits,
 # using the digits only once (ignore duplicates).
 # Only positive integers in the range of 1 to 9 will be passed to the function.
 
 def min_value(digits):
-    # choosing to use sort() inplace, it returns None instead of the sorted list assigned to a var
     set_digits = set(digits)
-    print(set_digits)
-    digit_list =list(set_digits)
+    # list creates a temp list, need to assign to a var
+    digit_list = list(set_digits)
+    # choosing to use sort() inplace, it returns None instead of the sorted list assigned to a var
     digit_list.sort()
-    print(set_digits)
-    #converting list to a string and returning int type
+    # converting a list to a string and returning an int type
     return int("".join([str(i) for i in digit_list]))
+
 
 print(min_value([4, 8, 1, 4]))
